8897fe1b52482261120bc986acd526578be79f64
engrromjenatlas-arch <engr.romjenatlas@gmail.com>
2026-02-23 21:29:37 +0800

    Add beam moment calculator

---
+++ main.py
@@ -0,0 +1,6 @@
+# Beam Moment Calculator 
+# Simply Supported Beam with Central Point Load 
+P = float (input (" Enter load (P): "))
+L = float (input ("Enter beam length (L): "))
+M = (P * L) / 4 
+print (" Maximum Bending Moment =" , M)  
